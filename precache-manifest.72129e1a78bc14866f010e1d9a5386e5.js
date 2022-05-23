fdb110f887b1a7d00e9b3380d0a2dfee20b4b6f2
Arnault <arnault.lalloz@gmail.com>
2022-05-23 15:15:07 +0200

    Updates

---
+++ precache-manifest.72129e1a78bc14866f010e1d9a5386e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef5deea24ccb363eb9a9f3d4b2effff",
+    "url": "/parkT/index.html"
+  },
+  {
+    "revision": "fe3012b7dc599a39b883",
+    "url": "/parkT/static/css/main.2a378707.chunk.css"
+  },
+  {
+    "revision": "fba94dfaa12d61e9e9fb",
+    "url": "/parkT/static/js/2.9c15448e.chunk.js"
+  },
+  {
+    "revision": "fe3012b7dc599a39b883",
+    "url": "/parkT/static/js/main.f9817f46.chunk.js"
+  },
+  {
+    "revision": "f47fc217b1cc6a09ad30",
+    "url": "/parkT/static/js/runtime-main.586eb419.js"
+  }
+]);
