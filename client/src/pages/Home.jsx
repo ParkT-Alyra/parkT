9386b90742849ce7034f4bba53ce4bcc0d3a34c0
TPDQ3633 <kevin.colonna@orange.com>
2022-05-30 21:32:35 +0200

    mise à jour du home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import TypeAnimation from 'react-type-animation';
 
 function Home() {
     return (
-        <h1>Optimisez vos revenus tout en préservant la planète c'est possible avec parkT</h1>
+        <TypeAnimation
+            cursor={false}
+            sequence={['Optimisez vos revenus tout en préservant la planète', 1000, 'C\'est possible avec parkT !', 1000]}
+            wrapper="h1"
+            repeat={10}
+            className="title mt-5 has-text-white"
+        />
     );
 }
 
