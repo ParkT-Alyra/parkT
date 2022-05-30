ced9be0d900ffd27710b0a5a2553204076b354ec
TPDQ3633 <kevin.colonna@orange.com>
2022-05-30 11:21:48 +0200

    ajout logo et navbar

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import logo from './logo_parkT.png' // relative path to image
 
 function Navbar(props) {
-  const { accounts, accountBalance } = props;
+  const { accounts } = props;
   return (
     <nav className="navbar is-warning" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <Link to="/parkT/">
-          <img src={logo} width="112" height="28"/>
+          <img src={logo} alt="parkT-logo" width="112" height="28"/>
         </Link>
       </div>
       <div className="navbar-menu ml-1">
